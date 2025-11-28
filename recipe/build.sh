bfa7015ebcf1c74757930c9ed5691ddaaa540bb8
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-28 01:36:06 -0800

    revert fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,6 @@ export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 export OPENSSL_DIR=${PREFIX}
 export OPENSSL_NO_VENDOR=1
-export LIBGIT2_NO_VENDOR=1
-
-export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
 
 cd src
 cargo-bundle-licenses \
