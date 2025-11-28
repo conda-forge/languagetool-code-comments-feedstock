4de02eab9a44ca9ad8745a0f9c96d09d4937a802
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-27 22:45:50 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,10 +6,14 @@ export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 export OPENSSL_DIR=${PREFIX}
 export OPENSSL_NO_VENDOR=1
+export LIBGIT2_NO_VENDOR=1
 
+export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+
+cd src
 cargo-bundle-licenses \
     --format yaml \
-    --output THIRDPARTY.yml
+    --output ${SRC_DIR}/THIRDPARTY.yml
 
 # build statically linked binary with Rust
 cargo install --bins --no-track --locked --root ${PREFIX} --path .
