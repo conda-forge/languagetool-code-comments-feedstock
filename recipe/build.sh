788fc1ab8580d902c36ab1d5befb4df42a64f7b9
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-12 22:15:02 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,14 @@ export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 export OPENSSL_DIR=${PREFIX}
 export OPENSSL_NO_VENDOR=1
+export LIBGIT2_NO_VENDOR=1
+export PKG_CONFIG_PATH="${BUILD_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
+export PKG_CONFIG_ALLOW_CROSS=1
+
+if [[ ${target_platform} =~ .*osx.* ]]; then
+    mkdir -p ${SRC_DIR}/target/release/deps
+    ln -sf ${BUILD_PREFIX}/lib/libgit2* ${SRC_DIR}/target/release/deps
+fi
 
 cd src
 cargo-bundle-licenses \
